ce48ea31e24bdcdd67abd586599b7c34d80de578
Param Aggarwal <paramaggarwal@gmail.com>
2014-03-08 16:20:40 +0530

    fetches venue data from foursquare

--- components/title.js
+++ components/title.js
@@ -7,17 +7,14 @@ var ReactRouter = require('react-router-component');
 
 var Link        = ReactRouter.Link;
 
-var SearchBox = require('../components/searchbox');
-var Title = require('../components/title');
-
 module.exports = React.createClass({
 
   render: function() {
     return (
-      <div className="MainPage container">
-        <Title />
-        <SearchBox />
-      </div>
+		<div className="title">
+		  <h1><Link href="/">Headto</Link></h1>
+		  <h2>Tell your friends where you are heading.</h2>
+		</div>
     );
   }
 });
--- pages/venue.js
+++ pages/venue.js
@@ -9,22 +9,39 @@ var ReactAsync  = require('react-async');
 var superagent  = require('superagent');
 var Link = ReactRouter.Link;
 
+var SearchBox = require('../components/searchbox');
+var Title = require('../components/title');
+
+var foursquare = require('../lib/foursquare');
+
 module.exports = React.createClass({
   mixins: [ReactAsync.Mixin],
 
   getInitialStateAsync: function(cb) {
-    superagent.get(
-      'http://localhost:3000/api/venue/' + this.props.id,
-      function(err, res) {
-        cb(err, res ? res.body : null);
-      });
+    foursquare.getVenue(this.props.id, function(err, res) {
+      if (err) {
+        console.log('Error', err);
+      } else {
+        cb(err, res);
+      }
+    });
   },
 
   render: function() {
     return (
-      <div className="VenuePage">
-        <h1>Hello, {this.state.name}!</h1>
-        <p><Link href="/">Logout</Link></p>
+      <div className="VenuePage container">
+        <Title />
+        <h1>{ this.state.venue.name }</h1>
+        <strong>{ this.state.venue.categories[0].name }</strong>
+        <p>{ this.state.venue.location.address }, { this.state.venue.location.city }.</p>
+        <p><a id="postHeadto">I am heading here</a></p>
+        <p><img src={'//a.tiles.mapbox.com/v3/paramaggarwal.map-mkz04dpf/pin-m-star('
+          + this.state.venue.location.lng + ','
+          + this.state.venue.location.lat + ')/'
+          + this.state.venue.location.lng + ','
+          + this.state.venue.location.lat + ','
+          + '14/400x200.png'} /></p>
+        <pre>{ JSON.stringify(this.state, null, 4) }</pre>
       </div>
     );
   }
